ec313d59cc4daa1a98a4fb7b81b097d5b4245b3d
Peter Mei <petermei@yahoo.com>
2018-01-23 19:33:50 -0800

    Update

--- ConsoleAppDynamoDB/Program.cs
+++ ConsoleAppDynamoDB/Program.cs
@@ -3,13 +3,186 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.Runtime;
+using Amazon.DynamoDBv2.Model;
 
 namespace ConsoleAppDynamoDB
 {
     class Program
     {
+        private static AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        private static string tableName = "ExampleTable";
+
         static void Main(string[] args)
         {
+            //var tableResponse = client.ListTables();
+            //List<string> tables = tableResponse.TableNames;
+            try
+            {
+                CreateExampleTable();
+                ListMyTables();
+                GetTableInformation();
+                UpdateExampleTable();
+
+                DeleteExampleTable();
+
+                Console.WriteLine("To continue, press Enter");
+                Console.ReadLine();
+            }
+            catch (AmazonDynamoDBException e) { Console.WriteLine(e.Message); }
+            catch (AmazonServiceException e) { Console.WriteLine(e.Message); }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+        }
+        private static void CreateExampleTable()
+        {
+            Console.WriteLine("\n*** Creating table ***");
+            var request = new CreateTableRequest
+            {
+                AttributeDefinitions = new List<AttributeDefinition>()
+            {
+                new AttributeDefinition
+                {
+                    AttributeName = "Id",
+                    AttributeType = "N"
+                },
+                new AttributeDefinition
+                {
+                    AttributeName = "ReplyDateTime",
+                    AttributeType = "N"
+                }
+            },
+                KeySchema = new List<KeySchemaElement>
+            {
+                new KeySchemaElement
+                {
+                    AttributeName = "Id",
+                    KeyType = "HASH" //Partition key
+                },
+                new KeySchemaElement
+                {
+                    AttributeName = "ReplyDateTime",
+                    KeyType = "RANGE" //Sort key
+                }
+            },
+                ProvisionedThroughput = new ProvisionedThroughput
+                {
+                    ReadCapacityUnits = 5,
+                    WriteCapacityUnits = 6
+                },
+                TableName = tableName
+            };
+
+            var response = client.CreateTable(request);
+
+            var tableDescription = response.TableDescription;
+            Console.WriteLine("{1}: {0} \t ReadsPerSec: {2} \t WritesPerSec: {3}",
+                      tableDescription.TableStatus,
+                      tableDescription.TableName,
+                      tableDescription.ProvisionedThroughput.ReadCapacityUnits,
+                      tableDescription.ProvisionedThroughput.WriteCapacityUnits);
+
+            string status = tableDescription.TableStatus;
+            Console.WriteLine(tableName + " - " + status);
+
+            WaitUntilTableReady(tableName);
+        }
+
+        private static void ListMyTables()
+        {
+            Console.WriteLine("\n*** listing tables ***");
+            string lastTableNameEvaluated = null;
+            do
+            {
+                var request = new ListTablesRequest
+                {
+                    Limit = 2,
+                    ExclusiveStartTableName = lastTableNameEvaluated
+                };
+
+                var response = client.ListTables(request);
+                foreach (string name in response.TableNames)
+                    Console.WriteLine(name);
+
+                lastTableNameEvaluated = response.LastEvaluatedTableName;
+            } while (lastTableNameEvaluated != null);
+        }
+
+        private static void GetTableInformation()
+        {
+            Console.WriteLine("\n*** Retrieving table information ***");
+            var request = new DescribeTableRequest
+            {
+                TableName = tableName
+            };
+
+            var response = client.DescribeTable(request);
+
+            TableDescription description = response.Table;
+            Console.WriteLine("Name: {0}", description.TableName);
+            Console.WriteLine("# of items: {0}", description.ItemCount);
+            Console.WriteLine("Provision Throughput (reads/sec): {0}",
+                      description.ProvisionedThroughput.ReadCapacityUnits);
+            Console.WriteLine("Provision Throughput (writes/sec): {0}",
+                      description.ProvisionedThroughput.WriteCapacityUnits);
+        }
+
+        private static void UpdateExampleTable()
+        {
+            Console.WriteLine("\n*** Updating table ***");
+            var request = new UpdateTableRequest()
+            {
+                TableName = tableName,
+                ProvisionedThroughput = new ProvisionedThroughput()
+                {
+                    ReadCapacityUnits = 6,
+                    WriteCapacityUnits = 7
+                }
+            };
+
+            var response = client.UpdateTable(request);
+
+            WaitUntilTableReady(tableName);
+        }
+
+        private static void DeleteExampleTable()
+        {
+            Console.WriteLine("\n*** Deleting table ***");
+            var request = new DeleteTableRequest
+            {
+                TableName = tableName
+            };
+
+            var response = client.DeleteTable(request);
+
+            Console.WriteLine("Table is being deleted...");
+        }
+
+        private static void WaitUntilTableReady(string tableName)
+        {
+            string status = null;
+            // Let us wait until table is created. Call DescribeTable.
+            do
+            {
+                System.Threading.Thread.Sleep(5000); // Wait 5 seconds.
+                try
+                {
+                    var res = client.DescribeTable(new DescribeTableRequest
+                    {
+                        TableName = tableName
+                    });
+
+                    Console.WriteLine("Table name: {0}, status: {1}",
+                              res.Table.TableName,
+                              res.Table.TableStatus);
+                    status = res.Table.TableStatus;
+                }
+                catch (ResourceNotFoundException)
+                {
+                    // DescribeTable is eventually consistent. So you might
+                    // get resource not found. So we handle the potential exception.
+                }
+            } while (status != "ACTIVE");
         }
     }
 }
